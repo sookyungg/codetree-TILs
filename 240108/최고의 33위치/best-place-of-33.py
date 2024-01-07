d5c5aa7a0cab88a63268402cdc30722dbab6f425
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-08 08:22:23 +0900

    Title: 최고의 33위치, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/최고의 33위치/best-place-of-33.py
+++ 240108/최고의 33위치/best-place-of-33.py
@@ -5,15 +5,14 @@ arr=[[0]*n for _ in range(n)]
 for i in range(n):
     arr[i]=list(map(int, input().split()))
 
-for i in range(n):
-    for j in range(n):
+for i in range(n-2):
+    for j in range(n-2):
         tmp_ans=0
-        if i+2<n and j+2<n:
-            for n in range(3):
-                for m in range(3):
-                    tmp_ans+=arr[i+n][j+m]
+        for n in range(3):
+            for m in range(3):
+                tmp_ans+=arr[i+n][j+m]
                 
-            answer=max(tmp_ans,answer)    
+        answer=max(tmp_ans,answer)    
         
         
 print(answer)
