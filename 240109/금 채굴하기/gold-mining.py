fe9cc276eaa61b0d1b8a8455fb6eb9f07d1b6fbf
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-09 04:57:01 +0900

    Title: 금 채굴하기, Time: 157ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240109/금 채굴하기/gold-mining.py
+++ 240109/금 채굴하기/gold-mining.py
@@ -3,21 +3,6 @@ arr=[[0]*n for _ in range(n)]
 for i in range(n):
     arr[i]=list(map(int,input().split()))
 
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-
-#최대 k 값 구하는 함수
-def maxk(n):
-    k=1
-    while True:
-        if n*n<=(k*k+(k+1)*(k+1)):
-            break
-        else:
-            k+=1
-    return k
-
-K=maxk(n)
-
 tax=0
 
 def goldcount(r,c,k):
