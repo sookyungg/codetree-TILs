9cc5fe8e3447326191584156473d884be87db2ae
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-09 05:01:45 +0900

    Title: 금 채굴하기, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240109/금 채굴하기/gold-mining.py
+++ 240109/금 채굴하기/gold-mining.py
@@ -3,12 +3,11 @@ arr=[[0]*n for _ in range(n)]
 for i in range(n):
     arr[i]=list(map(int,input().split()))
 
-tax=0
-
 def goldcount(r,c,k):
     gold=0
     for x in range(n):
         for y in range(n):
+            #맨해튼 거리
             if abs(r-x)+abs(c-y)<=k:
                 gold+=arr[x][y]
 
@@ -19,7 +18,7 @@ for x in range(n):
     for y in range(n):
         for k in range(n):
             tax=k*k+(k+1)*(k+1)
-            if goldcount(x,y,k)*m >=tax and maxvalue < goldcount(x,y,k):
+            if goldcount(x,y,k)*m-tax>=0 and maxvalue < goldcount(x,y,k):
                 maxvalue=goldcount(x,y,k)
 
 print(maxvalue)
