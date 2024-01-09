db4ff6879c2f3cceb8f04ad10614f31e6da0dac0
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-10 06:25:48 +0900

    Title: 겹치지 않게 선분 고르기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240110/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,40 @@
+n=int(input())
+line_arr=[]
+arr=[]
+ans=[]
+
+for i in range(n):
+    line_arr.append(list(map(int, input().split())))
+line_arr.sort()
+
+#이어지는지 체크
+
+def line_chk(arr):
+    tmp=[]
+    for i in range(len(arr)):
+        if arr[i]==1:
+            tmp.append(line_arr[i])
+    #print(tmp)
+    flag=0
+    for i in range(len(tmp)-1):
+        if tmp[i][-1] > tmp[i+1][0]:
+            flag=1
+    if flag==0:
+        ans.append(sum(arr))
+    
+    
+
+#조합 만들기
+def line_list(cur):
+    if cur == n:
+        #print(arr)
+        line_chk(arr)
+        return 
+    for i in range(2):
+        arr.append(i)
+        line_list(cur+1)
+        arr.pop()
+    return
+
+line_list(0)        
+print(max(ans))
