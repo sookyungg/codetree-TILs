40c16f39c6393dea53e7763fa5c3c783bf47a176
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-10 07:15:47 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240110/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240110/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -3,20 +3,19 @@ arr=[]
     
 def makearr(cur):
     if cur==n:
-        #print(arr)
-        #print([arr[0]]*n)
-        
-        if len(arr)>=3 and [arr[0]]*n!=arr:
-            #print(arr)
-            for a in arr:
-                print(a, end=' ')
-            print()
-        if len(arr)<=2:
-            for a in arr:
-                print(a, end=' ')
-            print()
-
+        cnt = 1
+        for i in range(n-1):
+            if arr[i] == arr[i+1]:
+                cnt += 1
+                if cnt == 3:
+                    return
+            else:
+                cnt = 1
+        for el in nums:
+            print(el, end=" ")
+        print()
         return 
+
     for i in range(1,k+1):
         arr.append(i)
         makearr(cur+1)
