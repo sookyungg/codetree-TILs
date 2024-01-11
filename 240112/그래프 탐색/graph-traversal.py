24705cec60edf14015b685d9723530a2d23b80c7
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-12 06:25:20 +0900

    Title: 그래프 탐색, Time: 171ms, Memory: 26MB, Status: Passed - CodeTree

--- 240112/그래프 탐색/graph-traversal.py
+++ 240112/그래프 탐색/graph-traversal.py
@@ -3,25 +3,23 @@
 # 방문했는지 확인, if 조건 확인
 # 1. 시작점으로부터 연결된 모든 정점을 전부 방문해야 합니다.
 # 2. 이미 방문한 정점은 다시는 방문하지 않습니다.
-
-n,m=map(int,input().split())
+n,m=map(int,input().split(' '))
 graph = [[] for _ in range(n+1)]
-visited = [[0] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
 res=0
 
 for _ in range(m):
-    x,y=map(int, input().split(' '))
+    x,y=map(int,input().split(' '))
     graph[x].append(y)
     graph[y].append(x)
 
-
 def dfs(v):
     global res
-    for cur in graph[v]:
-        if not visited[cur]:
-            visited[cur]=True
+    for cur_v in graph[v]:
+        if not visited[cur_v]:
+            visited[cur_v]=True
             res+=1
-            dfs(cur)
+            dfs(cur_v)
 
 visited[1]=True
 dfs(1)
