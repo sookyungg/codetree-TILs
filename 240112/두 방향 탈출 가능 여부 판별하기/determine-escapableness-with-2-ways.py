7eae3f02197580e09b35ad6e7930c84bcc25a507
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-12 07:49:43 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -9,14 +9,24 @@ for i in range(n):
 dx=[1,0]
 dy=[0,1]
 
-def dfs(x,y):
-    visited[x][y]=1
+def dfs(x, y):
+
+    visited[x][y] = 1
+
     for i in range(2):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0<=nx<n and 0<=ny<m: 
-            if visited[x][y]!=1 and arr[x][y]!=0:
-                dfs(nx,ny)
+        nx , ny = x + dx[i], y + dy[i]
+        if can_go(nx,ny):
+            dfs(nx,ny)
+
+
+def can_go(x,y):
+    if not(0 <= x and x < n and 0 <= y and y < m):
+        return False
+    
+    if visited[x][y] or arr[x][y] == 0:
+        return False
+    
+    return True
         
 dfs(0,0)
 #print(visited)
