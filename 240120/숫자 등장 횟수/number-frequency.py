42af9569d7955b70d8c72ec895c434ee6218c23e
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-20 09:06:14 +0900

    Title: 숫자 등장 횟수, Time: 180ms, Memory: 34MB, Status: Passed - CodeTree

--- 240120/숫자 등장 횟수/number-frequency.py
+++ 240120/숫자 등장 횟수/number-frequency.py
@@ -1,6 +1,15 @@
 n,m=map(int,input().split())
 arr=list(map(int,input().split()))
 num=list(map(int,input().split()))
+d=dict()
+for i in range(n):
+    if arr[i] in d:
+        d[arr[i]]+=1
+    else:
+        d[arr[i]]=1
 
 for i in range(m):
-    print(arr.count(num[i]), end=' ')
+    if num[i] in d:
+        print(d[num[i]], end=' ')
+    else:
+        print(0, end=' ')
