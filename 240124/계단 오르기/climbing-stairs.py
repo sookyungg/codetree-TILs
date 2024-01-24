2d522b30d3ab4dac93344cd313fd19ca2d87c7a8
Sookyung Kang <crab11@sogang.ac.kr>
2024-01-24 21:33:40 +0900

    Title: 계단 오르기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240124/계단 오르기/climbing-stairs.py
@@ -0,0 +1,17 @@
+n=int(input())
+MOD = 1000000007
+
+dp=[0]*(n+1)
+
+def stair(n):
+    dp[0]=1
+    dp[1]=1
+
+    for i in range(1,n+1):
+        if i>=2:
+            dp[i] = (dp[i] + dp[i - 2]) % MOD
+        if i>=3:
+            dp[i] = (dp[i] + dp[i - 3]) % MOD
+
+stair(n)
+print(dp[n])
