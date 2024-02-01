2d3b27da844ae2c879148d6d87c9baf0826c086c
Sookyung Kang <crab11@sogang.ac.kr>
2024-02-02 05:03:16 +0900

    Title: 사각형 채우기 3, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/사각형 채우기 3/rectangle-fill-3.py
+++ 240202/사각형 채우기 3/rectangle-fill-3.py
@@ -1,15 +1,20 @@
+MOD = 1000000007
+
+# 변수 선언 및 입력:
 n = int(input())
 
-dp = [0 for _ in range(n+2)]
+dp = [0] * (n + 1)
+
+# 초기 조건 설정
 dp[0] = 1
 dp[1] = 2
-dp[2] = 7
 
-if n <= 2:
-    print(dp[n])
-    exit()
+# 점화식에 따라 dp값 채우기
+# dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+#         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+for i in range(2, n + 1):
+    dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD
+    for j in range(i - 3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MOD
 
-for i in range(3, n+1):
-    dp[i] = (dp[i-1]*3 + dp[i-2] - dp[i-3]) % 1000000007
-
-print(dp[n] % 1000000007)
+print(dp[n])
