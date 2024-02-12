c1a5370a1e51a9ff5e195e70bdc92d9604b5fdfd
Sookyung Kang <crab11@sogang.ac.kr>
2024-02-13 04:18:23 +0900

    Title: 정수 선언하고 뺄셈, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240213/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a;
+        int b;
+        a=97;
+        b=13;
+        System.out.print(a+" - "+ b +" = "+ (a-b));
+    }
+}
