82a217f8df2a6b9ca7833f9228f271499e0944cf
Sookyung Kang <crab11@sogang.ac.kr>
2024-02-28 21:37:15 +0900

    Title: 거꾸로 순열, Time: 1056ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240228/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n;
+	static int[] visited;
+	static ArrayList<Integer> ans=new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+
+		visited=new int[n+1];
+		choose(1);
+
+	}
+	static void choose(int cur){
+		if(cur==n+1){
+			for(int j=0;j<n;j++){
+				System.out.print(ans.get(j)+" ");
+			}
+			System.out.print("\n");
+			return;
+		}
+		for(int i=n;i>=1;i--){
+			if(visited[i]==1) continue;
+
+			visited[i]=1;
+			ans.add(i);
+
+			choose(cur+1);
+
+			ans.remove(ans.size()-1);
+			visited[i]=0;
+		}
+
+	}
+
+
+
+}
