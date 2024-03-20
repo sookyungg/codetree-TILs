350b6231240b2ee7456e29287ef49c0583382fea
Sookyung Kang <crab11@sogang.ac.kr>
2024-03-20 13:35:29 +0900

    Title: 왕실의 기사 대결, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240320/왕실의 기사 대결/royal-knight-duel.py
+++ 240320/왕실의 기사 대결/royal-knight-duel.py
@@ -1,100 +1,98 @@
 from collections import deque
-import copy
 
-l,n,t=map(int,input().split())
-board=[list(map(int,input().split())) for _ in range(l)] # 맵 정보
+MAX_N=31
+MAX_L=41
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 
-info=[[0]*l for _ in range(l)]  # 맵 기사 정보
-move=[(-1,0),(0,1),(1,0),(0,-1)] # 이동
-knight=[[] for _ in range(n+1)] # 기사들 좌표
-blood=[0]
-origin_blood=[] 
+info=[[0]*MAX_L for _ in range(MAX_L)]
+origin_k=[0]*MAX_N
 
-init=[[0]*l for _ in range(l)] 
+r=[0]*MAX_N
+c=[0]*MAX_N
+h=[0]*MAX_N
+w=[0]*MAX_N
+k=[0]*MAX_N
 
-## init
-# 말 초기 정보
+nr=[0]*MAX_N
+nc=[0]*MAX_N
+dmg=[0]*MAX_N
+
+is_moved=[False]*MAX_N
+
+l,n,q=map(int,input().split())
+for i in range(1,l+1):
+    info[i][1:]=map(int,input().split())
+
 for i in range(1,n+1):
-    r,c,h,w,k=map(int,input().split())
-    blood.append(k)
-
-    info[r-1][c-1]=i
-    for j in range(h):
-        #info[r-1+j][c-1]=i
-        knight[i].append([r-1+j,c-1])
-
-    for k in range(1,w):
-        #info[r-1][c-1+k]=i
-        knight[i].append([r-1,c-1+k])
-
-origin_blood=copy.deepcopy(blood)
-new_knight=copy.deepcopy(knight)
+    r[i],c[i],h[i],w[i],k[i]=map(int,input().split())
+    origin_k[i]=k[i]
 
 
-
-for _ in range(t):
-    i,d=map(int,input().split())
+def try_movement(idx,d):
     q=deque()
-    q.append(i)
+    is_pos=True
 
-    info=copy.deepcopy(init)
-    for k in range(len(new_knight)):
-        for s in range(len(new_knight[k])):
-            info[new_knight[k][s][0]][new_knight[k][s][1]]=k
-   
-    flag=False
-    tmp=[]
+    #초기 세팅
+    for i in range(1,n+1):
+        dmg[i]=0
+        is_moved[i]=False
+        nr[i]=r[i]
+        nc[i]=c[i]
+
+    q.append(idx)
+    is_moved[idx]=True
+
     while q:
-        now_knight=q.popleft()
-        dx=move[d][0]
-        dy=move[d][1]
-        
-        for k in range(len(new_knight[now_knight])):
-            nx=new_knight[now_knight][k][0]+dx
-            ny=new_knight[now_knight][k][1]+dy
+        x=q.popleft()
+        nr[x]+=dx[d]
+        nc[x]+=dy[d]
 
+        #경계를 벗어나는지 체크 
+        if nr[x]<1 or nc[x]<1 or nr[x]+h[x]-1>l or nc[x]+w[x]-1>l:
+            return False
+
+        #벽이랑 충돌하는지 + dmg
+        for i in range(nr[x],nr[x]+h[x]):
+            for j in range(nc[x],nc[x]+w[x]):
+                if info[i][j]==1:
+                    dmg[x]+=1
+                if info[i][j]==2:
+                    return False
+
+       
+        
+        #다른 조각이랑 충돌하는지 충돌하면 append
+        for i in range(1,n+1):
+            if is_moved[i] or k[i]<=0:
+                continue
+            if r[i]>nr[x]+h[x]-1 or nr[x]>r[i]+h[i]-1:
+                continue
+            if c[i]>nc[x]+w[x]-1 or nc[x]>c[i]+w[i]-1:
+                continue
             
-            if 0<=nx<l and 0<=ny<l:
-                new_knight[now_knight][k][0]=nx
-                new_knight[now_knight][k][1]=ny
+            is_moved[i]=True
+            q.append(i)
 
-                if board[nx][ny]==2:
-                    flag=True
-                    break 
+    dmg[idx]=0
+    return True
 
-                elif board[nx][ny]!=2 and info[nx][ny]!=0:
-                    if info[nx][ny] not in q and info[nx][ny]!=now_knight:
-                        q.append(info[nx][ny])
-                        tmp.append(info[nx][ny])
-                
 
-            else:
-                flag=True
-                break
-        
-        if flag==True:
-            new_knight=copy.deepcopy(knight)
-            blood=copy.deepcopy(origin_blood) 
-            break
-        
-    
-    #print(i,d,new_knight,flag)
 
-   
-    if flag==False:
+for _ in range(q):
+    idx,d=map(int,input().split())
+    if k[idx]<=0:
+        continue
+    if try_movement(idx,d):
+        print(dmg)
+        for i in range(1,n+1):
+            r[i]=nr[i]
+            c[i]=nc[i]
+            k[i]-=dmg[i]
 
-        for k in range(1,len(new_knight)):
-            for s in range(len(new_knight[k])):
-                if board[new_knight[k][s][0]][new_knight[k][s][1]]==1 and k in tmp:
-                    blood[k]-=1
-                    if blood[k]<=0:
-                        new_knight[k]=[]
-#print(new_knight)   
-#print(origin_blood)
-#print(blood)
+print(origin_k)
+print(k)
 
-sum=0
-for i in range(len(origin_blood)):
-    if blood[i]>0:
-        sum+=origin_blood[i]-blood[i]
-print(sum)
+
+#ans=sum([origin_k[i]-k[i] for i in range(1,n+1) if k[i]>0])
+#print(ans)
