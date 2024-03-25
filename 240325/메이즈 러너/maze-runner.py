5997ae1dbb8edb2e2cbb25becee058603eed0559
Sookyung Kang <crab11@sogang.ac.kr>
2024-03-25 09:45:46 +0900

    Title: 메이즈 러너, Time: 184ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240325/메이즈 러너/maze-runner.py
+++ 240325/메이즈 러너/maze-runner.py
@@ -31,39 +31,55 @@ exit = list(map(int, input().split()))
 participant_move = 0
 
 
-def move(maze, participant, participant_move,exit):
+def isParticipantHere(r, c, boxsize, participant):
+    f = False
+    for i in range(len(participant)):
+        if r <= participant[i][0] <= r + boxsize and c <= participant[i][1] <= c + boxsize:
+            f = True
+    if f:
+        return True
+    else:
+        return False
+
+
+def move(maze, participant, participant_move, exit):
     participant_exit = [False] * (len(participant))
+
+    # 만일 현재 위치가 출구라면,
+    for i in range(len(participant)):
+        px = participant[i][0]
+        py = participant[i][1]
+        if px == exit[0] and py == exit[1]:
+            participant_exit[i] = True
+
     # 모든 참가자들 이동
     for i in range(len(participant)):
         # 현재 참가자 위치
-            px = participant[i][0]
-            py = participant[i][1]
+        px = participant[i][0]
+        py = participant[i][1]
 
-            min_length = calculate(px, py, exit[0], exit[1])
-            min_dir = -1
-            for j in range(4):
-                nx = px + dx[j]
-                ny = py + dy[j]
-                # 움직일 수 있는지 체크
-                if isRange(nx, ny) and maze[nx][ny] == 0:
-                    length = calculate(nx, ny, exit[0], exit[1])
-                    if length < min_length:
-                        min_length = length
-                        min_dir = j
-            if min_dir != -1:
-                participant[i][0] += dx[min_dir]
-                participant[i][1] += dy[min_dir]
-                participant_move += 1
-                if participant[i][0] == exit[0] and participant[i][1] == exit[1]:
-                    participant_exit[i] = True
+        min_length = calculate(px, py, exit[0], exit[1])
+        min_dir = -1
+        for j in range(4):
+            nx = px + dx[j]
+            ny = py + dy[j]
+            # 움직일 수 있는지 체크
+            if isRange(nx, ny) and maze[nx][ny] == 0:
+                length = calculate(nx, ny, exit[0], exit[1])
+                if length < min_length:
+                    min_length = length
+                    min_dir = j
+        if min_dir != -1:
+            participant[i][0] += dx[min_dir]
+            participant[i][1] += dy[min_dir]
+            participant_move += 1
+            if participant[i][0] == exit[0] and participant[i][1] == exit[1]:
+                participant_exit[i] = True
 
-    new_participant =[]
+    new_participant = []
     for i in range(len(participant)):
         if not participant_exit[i]:
             new_participant.append(participant[i])
-
-
-
 
     return new_participant, participant_move
 
@@ -106,20 +122,19 @@ def rotation(maze, participant, exit):
             box_r = exit[0] - i
             box_c = exit[1] - j
             if isRange(box_r, box_c):
-                if box_r <= participant[min_par][0] <= box_r + boxsize and box_c <= participant[min_par][1] <= box_c + boxsize:
-                    if min_box_r > box_r:
-                        min_box_r = box_r
-                        min_box_c = box_c
-                    elif min_box_r == box_r:
+                if isParticipantHere(box_r,box_c,boxsize,participant):
+                    if min_box_r == box_r:
                         if min_box_c > box_c:
                             min_box_r = box_r
                             min_box_c = box_c
+                    elif min_box_r > box_r:
+                        min_box_r = box_r
+                        min_box_c = box_c
 
     # 1. maze에 참가자 위치 채움
     for i in range(len(participant)):
         maze[participant[i][0]][participant[i][1]] = 999
     maze[exit[0]][exit[1]] = -1
-
 
     new_maze = copy.deepcopy(maze)
 
@@ -128,13 +143,12 @@ def rotation(maze, participant, exit):
     y = min_box_c + boxsize + 1
     for i in range(min_box_r, min_box_r + boxsize + 1):
         x = min_box_r
-        y-=1
+        y -= 1
         for j in range(min_box_c, min_box_c + boxsize + 1):
             if maze[i][j] > 0 and maze[i][j] != 999:
                 maze[i][j] -= 1
             new_maze[x][y] = maze[i][j]
-            x+=1
-
+            x += 1
 
     maze = copy.deepcopy(new_maze)
     participant = []
@@ -144,26 +158,27 @@ def rotation(maze, participant, exit):
             if maze[i][j] == 999:
                 maze[i][j] = 0
                 participant.append([i, j])
-            if maze[i][j]==-1:
+            if maze[i][j] == -1:
                 maze[i][j] = 0
-                exit=(i,j)
+                exit = (i, j)
 
     return participant, maze, exit
 
-flag=False
+
+flag = False
 for _ in range(k):
     participant, participant_move = move(maze, participant, participant_move, exit)
     # 게임 끝났는지 확인
     if isGameEnd(participant):
         print(participant_move)
         for i in exit:
-            print(i,end=" ")
-        flag=True
+            print(i, end=" ")
+        flag = True
         break
     else:
         participant, maze, exit = rotation(maze, participant, exit)
         # print(maze)
-if flag!=True:
+if flag != True:
     print(participant_move)
     for i in exit:
         print(i, end=" ")
